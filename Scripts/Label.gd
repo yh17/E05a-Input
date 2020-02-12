8177a85a8f4a994cd1982b7a76fa772a2c7f96f7
yh17 <60178265+yh17@users.noreply.github.com>
2020-02-12 17:57:57 -0500

    Complete the assignment

--- Scripts/Label.gd
+++ Scripts/Label.gd
@@ -5,4 +5,6 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+ 	var mouse_pos = get_viewport().get_mouse_position()
+  print(mouse_pos.x)
+ 	text = str(mouse_pos.x) + ", " + str(mouse_pos.y)
